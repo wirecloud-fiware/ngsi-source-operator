9f97a83b861d77739ca7e67a688411aa60b217f7
Alejandro Rodriguez <alex.rodriguez@conwet.com>
2016-12-16 11:08:40 +0100

    Update repo structure

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,7 @@
-/*!
- *   Copyright 2014-2016 CoNWeT Lab., Universidad Politecnica de Madrid
- *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
+/*
+ * Copyright (c) 2016 Vendor
+ * Licensed under the MIT license.
  */
-
 
 var ConfigParser = require('wirecloud-config-parser');
 var parser = new ConfigParser('src/config.xml');
@@ -26,6 +14,24 @@ module.exports = function (grunt) {
 
         metadata: parser.getData(),
 
+        eslint: {
+            operator: {
+                src: 'src/js/**/*.js',
+            },
+            grunt: {
+                options: {
+                    configFile: '.eslintrc-node'
+                },
+                src: 'Gruntfile.js',
+            },
+            test: {
+                options: {
+                    configFile: '.eslintrc-jasmine'
+                },
+                src: ['src/test/**/*.js', '!src/test/fixtures/']
+            }
+        },
+
         copy: {
             main: {
                 files: [
@@ -37,11 +43,18 @@ module.exports = function (grunt) {
         strip_code: {
             multiple_files: {
                 src: ['build/src/js/**/*.js']
+            },
+            imports: {
+                options: {
+                    start_comment: 'import-block',
+                    end_comment: 'end-import-block'
+                },
+                src: ['src/js/*.js']
             }
         },
 
         compress: {
-            widget: {
+            operator: {
                 options: {
                     mode: 'zip',
                     archive: 'dist/<%= metadata.vendor %>_<%= metadata.name %>_<%= metadata.version %>.wgt'
@@ -51,10 +64,11 @@ module.exports = function (grunt) {
                         expand: true,
                         cwd: 'src',
                         src: [
+                            'DESCRIPTION.md',
                             'css/**/*',
                             'doc/**/*',
                             'images/**/*',
-                            'DESCRIPTION.md',
+                            'index.html',
                             'config.xml'
                         ]
                     },
@@ -92,67 +106,67 @@ module.exports = function (grunt) {
             }
         },
 
-        jscs: {
-            widget: {
-                src: 'src/js/**/*',
+        jasmine: {
+            test: {
+                src: ['src/js/*.js', '!src/js/main.js'],
                 options: {
-                    config: ".jscsrc"
+                    specs: 'src/test/js/*Spec.js',
+                    helpers: ['src/test/helpers/*.js'],
+                    vendor: [
+                        'node_modules/mock-applicationmashup/lib/vendor/mockMashupPlatform.js',
+                        'src/test/vendor/*.js'
+                    ]
                 }
             },
-            grunt: {
-                src: 'Gruntfile.js',
+            coverage: {
+                src: '<%= jasmine.test.src %>',
                 options: {
-                    config: ".jscsrc"
-                }
-            }
-        },
-
-        jshint: {
-            options: {
-                jshintrc: true
-            },
-            all: {
-                files: {
-                    src: ['src/js/**/*.js']
-                }
-            },
-            grunt: {
-                options: {
-                    jshintrc: '.jshintrc-node'
-                },
-                files: {
-                    src: ['Gruntfile.js']
+                    helpers: '<%= jasmine.test.options.helpers %>',
+                    specs: '<%= jasmine.test.options.specs %>',
+                    vendor: '<%= jasmine.test.options.vendor %>',
+                    template: require('grunt-template-jasmine-istanbul'),
+                    templateOptions: {
+                        coverage: 'build/coverage/json/coverage.json',
+                        report: [
+                            {type: 'html', options: {dir: 'build/coverage/html'}},
+                            {type: 'cobertura', options: {dir: 'build/coverage/xml'}},
+                            {type: 'text-summary'}
+                        ]
+                    }
                 }
             }
         },
 
         wirecloud: {
+            options: {
+                overwrite: false
+            },
             publish: {
-                file: 'build/<%= metadata.vendor %>_<%= metadata.name %>_<%= metadata.version %>.wgt'
+                file: 'dist/<%= metadata.vendor %>_<%= metadata.name %>_<%= metadata.version %>.wgt'
             }
         }
 
     });
 
     grunt.loadNpmTasks('grunt-wirecloud');
+    grunt.loadNpmTasks('grunt-contrib-jasmine'); // when test?
+    grunt.loadNpmTasks('gruntify-eslint');
     grunt.loadNpmTasks('grunt-contrib-compress');
     grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks("grunt-jscs");
     grunt.loadNpmTasks('grunt-strip-code');
+    grunt.loadNpmTasks('grunt-text-replace');
 
     grunt.registerTask('test', [
-        'jshint:grunt',
-        'jshint',
-        'jscs'
+        'eslint',
+        //'jasmine:coverage'
     ]);
 
     grunt.registerTask('build', [
         'clean:temp',
         'copy:main',
         'strip_code',
-        'compress:widget'
+        'compress:operator'
     ]);
 
     grunt.registerTask('default', [
@@ -164,4 +178,5 @@ module.exports = function (grunt) {
         'default',
         'wirecloud'
     ]);
+
 };
