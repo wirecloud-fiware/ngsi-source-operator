ca41ff87fb446974ec13f900aa73b0c6ef89449f
Álvaro Arranz <aarranz@ficodes.com>
2017-12-07 11:56:44 +0100

    Add more tests regarding operators preferences

--- tests/js/NGSISourceSpec.js
+++ tests/js/NGSISourceSpec.js
@@ -6,7 +6,7 @@
 
     describe("NGSI Source operator should", function () {
 
-        var operator;
+        var operator, abort_mock;
 
         beforeAll(function () {
             window.MashupPlatform = new MockMP({
@@ -31,14 +31,20 @@
             MashupPlatform.reset();
             MashupPlatform.resetData();
             operator = new NGSISource();
+            abort_mock = jasmine.createSpy('abort');
             window.NGSI = {
                 Connection: jasmine.createSpy('NGSI').and.callFake(function () {
                     this.v2 = {
-                        createSubscription: jasmine.createSpy('createSubscription'),
+                        createSubscription: jasmine.createSpy('createSubscription').and.callFake(function () {
+                            return Promise.resolve({subscription: {id: '5a291bb652c2f6bef3e02fd9'}});
+                        }),
+                        deleteSubscription: jasmine.createSpy('deleteSubscription').and.callFake(function () {
+                            return Promise.resolve();
+                        }),
                         listEntities: jasmine.createSpy('listEntities').and.callFake(function () {
                             var p = Promise.resolve();
                             p.then = () => {
-                                return {abort: function () {}};
+                                return {abort: abort_mock};
                             };
                             return p;
                         })
@@ -112,6 +118,65 @@
                 },
                 use_user_fiware_token: false
             });
+        });
+
+        it("updates ngsi connection on prefs change", () => {
+            MashupPlatform.operator.outputs.entityOutput.connect(true);
+
+            operator.init();
+            var initial_connection = operator.connection;
+            expect(initial_connection).not.toEqual(null);
+
+            MashupPlatform.prefs.simulate({
+                'ngsi_server': 'https://orion2.example.com',
+            });
+
+            expect(initial_connection.v2.deleteSubscription).not.toHaveBeenCalled();
+            expect(abort_mock).toHaveBeenCalled();
+            expect(operator.connection).not.toEqual(initial_connection);
+            expect(NGSI.Connection).toHaveBeenCalledWith('https://orion2.example.com', {
+                ngsi_proxy_url: 'https://ngsiproxy.example.com',
+                request_headers: {
+                    'FIWARE-Service': 'Tenant',
+                    'FIWARE-ServicePath': '/Spain/Madrid'
+                },
+                use_user_fiware_token: false
+            });
+        });
+
+        it("remove current subscription on prefs changes", (done) => {
+            MashupPlatform.operator.outputs.entityOutput.connect(true);
+            MashupPlatform.prefs.set('ngsi_update_attributes', 'location');
+
+            operator.init();
+            var initial_connection = operator.connection;
+            expect(initial_connection).not.toEqual(null);
+
+            // Wait until the subscription is created
+            setTimeout(() => {
+                expect(operator.subscriptionId).toEqual('5a291bb652c2f6bef3e02fd9');
+
+                MashupPlatform.prefs.simulate({
+                    'ngsi_server': 'https://orion2.example.com',
+                });
+
+                // Wait until subscription is deleted
+                setTimeout(() => {
+                    expect(initial_connection.v2.deleteSubscription).toHaveBeenCalled();
+                    expect(abort_mock).toHaveBeenCalled();
+                    expect(operator.connection).not.toEqual(initial_connection);
+                    expect(NGSI.Connection).toHaveBeenCalledWith('https://orion2.example.com', {
+                        ngsi_proxy_url: 'https://ngsiproxy.example.com',
+                        request_headers: {
+                            'FIWARE-Service': 'Tenant',
+                            'FIWARE-ServicePath': '/Spain/Madrid'
+                        },
+                        use_user_fiware_token: false
+                    });
+
+                    done();
+                }, 0);
+            }, 0);
         });
 
     });
